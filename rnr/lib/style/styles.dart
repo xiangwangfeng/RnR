451341cf22c4e72ffd6beeec93ae1416f79372d8
amao <xiangwangfeng@corp.netease.com>
2019-03-05 11:13:06 +0800

    update app icon

--- rnr/lib/style/styles.dart
+++ rnr/lib/style/styles.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 const Color kCalendarSelectCircleColor    = Color.fromARGB(255, 123, 191, 234);
 const Color kCalendarBackgroundColor      = Color.fromARGB(255, 30, 31, 32);
-const Color kCalendarTextHighlightColor   = Colors.red;
+const Color kCalendarTextHighlightColor   = Color.fromARGB(255, 123, 191, 234);
 const Color kCalendarTextNormalColor      = Colors.white;
 const Color kCalendarTextDisabledColor    = Colors.white24;
 const Color kCalendarTextSelectedColor    = Colors.black;
 const Color kCalendarFlagDoneColor        = Colors.green;
 const Color kCalendarFlagHalfColor        = Colors.blue;
 
-const Color kAppBackgroundColor =kCalendarBackgroundColor;
+const Color kAppBackgroundColor = kCalendarBackgroundColor;
 const Color kAppButtonBackgroundColor = kCalendarSelectCircleColor;
 
 
